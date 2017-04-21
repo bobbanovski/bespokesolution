6753b3e97bbcd1a40bb4f62a7d75076d4d94bec4
bobbanovski <bobbanovski@gmail.com>
2017-04-21 23:47:01 +0000

    setup home view

---
+++ solution/home/views.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, session, render_template
+
+home_app = Blueprint('home_app', __name__)
+
+@home_app.route('/')
+def home():
+    return render_template('home/home.html')
