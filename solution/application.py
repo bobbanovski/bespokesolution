6306c0c75233fcc7fb95524b76c2df81a31b6ee0
bobbanovski <bobbanovski@gmail.com>
2017-04-22 10:17:03 +0000

    remove settings.py

--- solution/application.py
+++ solution/application.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app(**config_overrides): #** allows unit tester to override
     app = Flask(__name__)
     
-    app.config.from_pyfile('settings.py') #get application settings from file settings.py
+    
     #Need to override this for unit testing
     app.config.update(config_overrides)
     
