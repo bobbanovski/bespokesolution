9e0ab85c15da7c8e4a726d1f387710d35f14e196
bobbanovski <bobbanovski@gmail.com>
2017-04-22 13:30:16 +0000

    add pandas to project, import RBA

--- solution/rba/api.py
+++ solution/rba/api.py
@@ -1,12 +1,14 @@
 from flask.views import MethodView
 from flask import jsonify, request, abort
+import pandas as pd
 
 class rbaApi(MethodView):
-    data = [
-        {"id": 1, "name": u"Rob"},
-        {"id": 2, "name": u"Jim"},
-        {"id": 3, "name": u"Rambo"}
-        ]
+    # data = [
+    #     {"id": 1, "name": u"Rob"},
+    #     {"id": 2, "name": u"Jim"},
+    #     {"id": 3, "name": u"Rambo"}
+    #     ]
+    data = pd.read_excel("http://www.rba.gov.au/statistics/tables/xls-hist/f16hist-2009-2015.xls").to_json()
         
     def get(self):
         return jsonify({"data": self.data})
