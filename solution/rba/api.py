85ab3e5ef3c84406beef3a82e0817193549e5d0b
bobbanovski <bobbanovski@gmail.com>
2017-04-23 01:40:35 +0000

    implement rba data download and merging

--- solution/rba/api.py
+++ solution/rba/api.py
@@ -1,14 +1,20 @@
 from flask.views import MethodView
 from flask import jsonify, request, abort
 import pandas as pd
+import openpyxl, xlrd
 
 class rbaApi(MethodView):
-    # data = [
-    #     {"id": 1, "name": u"Rob"},
-    #     {"id": 2, "name": u"Jim"},
-    #     {"id": 3, "name": u"Rambo"}
-    #     ]
-    data = pd.read_excel("http://www.rba.gov.au/statistics/tables/xls-hist/f16hist-2009-2015.xls").to_json()
-        
+    #download excel file from rba.gov.au, remove unwanted rows and columns
+    data1 = pd.read_excel("http://www.rba.gov.au/statistics/tables/xls-hist/f16hist-2009-2015.xls")[[0, 8]]
+    data1 = data1.iloc[10:]
+    data1.columns = ["Date", "FCMYJUN14D"]
+    data2 = pd.read_excel("http://www.rba.gov.au/statistics/tables/xls-hist/f16hist-2009-2015.xls", sheetname = 1)[[0,2]]
+    data2 = data2.iloc[10:]
+    data2.columns = ["Date", "FCMYJUN14D"]
+    
+    #join dataframes from 2 excel sheets and drop rows with null values
+    frames = [data1, data2]
+    result = pd.concat(frames).dropna().to_json(orient='records')
+    
     def get(self):
-        return jsonify({"data": self.data})
+        return jsonify({"data": self.result})
